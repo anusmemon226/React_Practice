126a19a25b1fd25c379a15037fb77610cdaca752
anusmemon <anusm226@gmail.com>
2023-06-22 22:56:25 +0500

    Second Practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 
 
-let name = "Anas";
+let name = "Anas Memon";
 
 function App() {
   return (
     <>
       <div className="main">
         <h1>My Name is {name}</h1>
-        <form action='http://localhost:80/getData.php' method='POST'>
-          <label htmlFor="userN">Username : </label>
-          <input type="text" name="username" id="userN" placeholder="Enter Your Name"/>
-          <input type="submit" name="submit"/>
-        </form>
-        <img src=""/>
       </div>
     </>
   );
